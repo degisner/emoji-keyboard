c0dc996bf76886204994649fe900e610ab46567b
Andrew <degisner@icloud.com>
2023-11-07 21:40:36 +0200

    Update UsageView.swift

--- UsageView.swift
+++ UsageView.swift
@@ -1,1 +1,22 @@
-
+ZStack {
+    EmojiTextField(text: $iconRaw)
+        .focused($isEmoji)
+        .frame(width: 24, height: 24)
+        .clipped()
+    
+    Circle()
+        .foregroundStyle(colorScheme == .light ? .white : .clear)
+    
+    Circle()
+        .foregroundStyle(color.asColor)
+        .opacity(0.4)
+        .opacity(color.asColor.inactive(colorScheme))
+        .animation(.default, value: color)
+    
+    Button {
+        isEmoji.toggle()
+    } label: {
+        Text(icon)
+            .font(.system(size: 54))
+    }
+}
